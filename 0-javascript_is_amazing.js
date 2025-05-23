f3c6e5b8183c8808e4d8d52b277b88bd1cdfc868
Juliet Njagi <njagijuliet02@gmail.com>
2025-05-23 18:44:35 +0300

    added new code to this file

--- 0-javascript_is_amazing.js
+++ 0-javascript_is_amazing.js
@@ -1,12 +1,2 @@
-/*Write a script that prints “JavaScript is amazing”:
-
-You must create a constant variable called myVar with the value “JavaScript is amazing”
-You must use console.log(...) to print all output
-You are not allowed to use var */
-
-
-// Declare a constant variable
 const myVar = "JavaScript is amazing";
-
-// Print the message to the console
 console.log(myVar);
