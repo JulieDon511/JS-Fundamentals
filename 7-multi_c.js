82a80c31f92a0d2516f85657fdb02dcc24493af3
Juliet Njagi <njagijuliet02@gmail.com>
2025-05-24 22:02:25 +0300

    changed count<=0 to count <0

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -2,7 +2,7 @@ const args = process.argv.slice(2);
 const count = parseInt(args[0]); 
 const a = 'C is fun'; 
 
-if (isNaN(count) || count <= 0) {
+if (isNaN(count) || count < 0) {
     console.log('Missing number of occurrences'); 
 } else {
     for (let i = 0; i < count; i++) { 
