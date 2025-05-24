ce8148d77a97137d31628c5bc4e72a87dc0123f6
Juliet Njagi <njagijuliet02@gmail.com>
2025-05-24 21:45:10 +0300

    changed the multi.cjs file added trim method

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,21 +1,13 @@
-
-
 const args = process.argv.slice(2);
 const count = parseInt(args[0]);
 const a = 'C is fun';
 
-if (isNaN(count) || count <= 0) { // Check if the conversion is valid and positive
-    console.log('Missing number of occurrences');
+if (isNaN(count) || count <= 0) {
+    console.log('Missing number of occurrences'); 
 } else {
-    for (let i = 0; i < count; i++) { // Use different variable 'i' for loop counter
-        console.log(a);
+    let output = ''; 
+    for (let i = 0; i < count; i++) {
+        output += a + '\n'; 
     }
+    console.log(output.trim()); 
 }
-
-
-
-
-
-
-
-
