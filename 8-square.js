0736f95c91675427e1eb620a6682329802c15477
Juliet Njagi <njagijuliet02@gmail.com>
2025-05-24 21:48:54 +0300

    changed the multi.cjs file added trim method

--- 8-square.js
+++ 8-square.js
@@ -1,19 +1,11 @@
+const args = process.argv.slice(2); 
+const size = parseInt(args[0]); 
+const character = 'X'; 
 
-
-const args =process.argv.slice(2);
-let side = parseInt(args[0]);
-const a = "X"
-
-
-
-if (isNaN(side) || side<=0 ){
-    console.log('Missing size');
-}else{
-   for( let i = 0; i<side; i++){
-
-
-    console.log(a.repeat(side));
-   }
-
-    
+if (isNaN(size) || size <= 0) {
+    console.log('Missing size'); 
+} else {
+    for (let i = 0; i < size; i++) { 
+        console.log(character.repeat(size)); s
+    }
 }
