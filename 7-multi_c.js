e4731c19163d3d594a456fd35a21a4c03f2a99aa
Juliet Njagi <njagijuliet02@gmail.com>
2025-05-24 21:59:15 +0300

    made changes on the entire code

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,13 +1,11 @@
-const args = process.argv.slice(2);
-const count = parseInt(args[0]);
-const a = 'C is fun';
+const args = process.argv.slice(2); 
+const count = parseInt(args[0]); 
+const a = 'C is fun'; 
 
 if (isNaN(count) || count <= 0) {
     console.log('Missing number of occurrences'); 
 } else {
-    let output = ''; 
-    for (let i = 0; i < count; i++) {
-        output += a + '\n'; 
+    for (let i = 0; i < count; i++) { 
+        console.log(a); 
     }
-    console.log(output.trim()); 
 }
