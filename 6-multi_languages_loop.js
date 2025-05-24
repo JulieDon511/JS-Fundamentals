2b2b5aec6ec1f5f5e09c7074f99760c06790646d
Juliet Njagi <njagijuliet02@gmail.com>
2025-05-24 21:39:36 +0300

    changed the console.log

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,7 +1,8 @@
-let i=0;
-const myArry = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
- while(i< myArry.length){
-    console.log(myArry[i]);
+const myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let i = 0;
+
+while (i < myArray.length) {
     i++;
- }
- 
+}
+
+console.log(myArray.join('\n')); // Print all lines in one console.log
