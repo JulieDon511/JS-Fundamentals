2b48725c8fcfccdd29cf0bf99c376d33bd00d556
Juliet Njagi <njagijuliet02@gmail.com>
2025-05-24 11:44:07 +0300

    Applied the process.argv concept

---
+++ 2-arguments.js
@@ -0,0 +1,20 @@
+const totalArgs = process.argv.length;
+
+if (totalArgs === 2){
+    console.log('No argument');
+}else if(totalArgs === 3){
+    console.log('Argument found');
+}else{
+    console.log('Arguments found');
+
+}
+
+
+
+
+
+
+
+
+
+
